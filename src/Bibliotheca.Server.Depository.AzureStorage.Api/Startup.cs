ef2ec9641e18804fba91fd9d898003d1a4281b92
Marcin Czachurski <mczachurski@icloud.com>
2017-01-02 18:22:14 +0100

    Change service discovery.

--- src/Bibliotheca.Server.Depository.AzureStorage.Api/Startup.cs
+++ src/Bibliotheca.Server.Depository.AzureStorage.Api/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Bibliotheca.Server.Depository.AzureStorage.Core.Parameters;
 using Bibliotheca.Server.Depository.AzureStorage.Core.Services;
 using Bibliotheca.Server.Depository.AzureStorage.Core.Validators;
@@ -125,28 +126,22 @@ namespace Bibliotheca.Server.Depository.AzureStorage.Api
         private void RegisterClient()
         {
             var serviceDiscoveryConfiguration = Configuration.GetSection("ServiceDiscovery");
-            var clientOptions = new ClientOptions
-            {
-                ServiceId = serviceDiscoveryConfiguration["ServiceId"],
-                ServiceName = serviceDiscoveryConfiguration["ServiceName"],
-                AgentAddress = serviceDiscoveryConfiguration["AgentAddress"],
-                Datacenter = serviceDiscoveryConfiguration["Datacenter"],
-                ClientPort = GetPort()
-            };
+
+            var tags = new List<string>();
+            var tagsSection = serviceDiscoveryConfiguration.GetSection("ServiceTags");
+            tagsSection.Bind(tags);
+
             var serviceDiscovery = new ServiceDiscoveryClient();
-            serviceDiscovery.Register(clientOptions);
-        }
-
-        private int GetPort()
-        {
-            var address = Configuration["server.urls"];
-            if (!string.IsNullOrWhiteSpace(address))
+            serviceDiscovery.Register((options) =>
             {
-                var url = new Uri(address);
-                return url.Port;
-            }
-
-            return 5000;
+                options.ServiceOptions.Id = serviceDiscoveryConfiguration["ServiceId"];
+                options.ServiceOptions.Name = serviceDiscoveryConfiguration["ServiceName"];
+                options.ServiceOptions.Address = serviceDiscoveryConfiguration["ServiceAddress"];
+                options.ServiceOptions.Port = Convert.ToInt32(serviceDiscoveryConfiguration["ServicePort"]);
+                options.ServiceOptions.HttpHealthCheck = serviceDiscoveryConfiguration["ServiceHttpHealthCheck"];
+                options.ServiceOptions.Tags = tags;
+                options.ServerOptions.Address = serviceDiscoveryConfiguration["ServerAddress"];
+            });
         }
     }
 }
